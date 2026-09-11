d85d913f561de7e2c173defd7e0107c5527b14de
Melai-rgb <k43608569@gmail.com>
2026-09-11 18:48:27 +0900

    구명보트

---
+++ junsik/4주차/구명보트/구명보트.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    int right = people.size()-1;
+    int left = 0;
+    int index = 0;
+    
+    sort(people.begin(), people.end(), less<int>());
+    
+    while(left <= right){
+        if(people[left] + people[right] <= limit){
+            left++;
+            right--;
+        }
+        
+        else{
+            right--;
+        }
+        answer++;
+    }
+    
+    return answer;
+}
